c8c22bbd42068de55660453060bd7bf6884e1822
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-13 11:01:42 +0100

    adds password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ require 'bcrypt'
 class User
   include DataMapper::Resource
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+  # validates_confirmation_of is a DataMapper method
+# provided especially for validating confirmation passwords!
+# The model will not save unless both password
+# and password_confirmation are the same
+# read more about it in the documentation
+# http://datamapper.org/docs/validations.html
+
+
   property :id, Serial
   property :email, String
   # this will store both the password and the salt
@@ -18,7 +28,9 @@ class User
   # has both the password hash and the salt. We save it to the
   # database instead of the plain password for security reasons.
   def password=(password)  # AV where/when is this method called?
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
+  validates_confirmation_of :password
 
 end
