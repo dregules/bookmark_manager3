e42eedcbb8eb6f8eaa9e7c61c0aed9c09ba4c6f0
Diego Regules <diegoregules@gmail.com>
2015-08-13 11:41:49 +0100

    sanitizing user inputs part 1

--- app/app.rb
+++ app/app.rb
@@ -1,13 +1,16 @@
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
 require_relative 'models/link'
+require 'sinatra/flash'
+
 class App < Sinatra::Base
 
+  register Sinatra::Flash
   enable :sessions
-  set :session_secret, 'super secret'  # i think this is a stub so that the session is always ecrypted the same way
+  set :session_secret, 'super secret'  # makes session play nicely with shotgun
 
   get '/' do
-    erb :'layout'
+    redirect('/links')
   end
 
   get '/links' do
@@ -39,8 +42,14 @@ class App < Sinatra::Base
 
   post '/users' do
     user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = 'Password and confirmation password do not match'
+      erb :'users/new'
+
+    end
   end
 
   helpers do
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -14,11 +14,13 @@ feature 'User sign up' do
     expect(User.first.email).to eq('alice@example.com')
   end
 
-  scenario 'requires a matching confirmation password' do
+  scenario 'with a password that does not match' do
     # again it's questionable whether we should be testing the model at this
-  # level.  We are mixing integration tests with feature tests.
-  # However, it's convenient for our purposes.
-  expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    # level.  We are mixing integration tests with feature tests.
+    # However, it's convenient for our purposes.
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 end
 
